c24e18698c1a98f4c38f2464319693dbacccbde5
Levitina Veronika <levitinava@gmail.com>
2018-10-19 10:03:15 +0300

    оптимизация картинок, webp

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -15,7 +19,16 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe((csso()))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"))
+  //  .pipe(server.stream());
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"))
 });
 
 gulp.task("server", function () {
@@ -31,4 +44,13 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
 gulp.task("start", gulp.series("css", "server"));
